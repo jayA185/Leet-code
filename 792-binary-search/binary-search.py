5dd5eea5e75b665a3abc9ceb0e7ad02e18db6868
Jay pandhurnekar <jaywpandhurnekar2006@jdcoem.ac.in>
2026-05-19 10:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (95.65%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums, target):
-        left, right = 0, len(nums) - 1
+        left = 0
+        right = len(nums) - 1
         
         while left <= right:
             mid = (left + right) // 2
@@ -12,4 +13,4 @@ class Solution:
             else:
                 right = mid - 1
                 
-        return -1   
+        return -1
