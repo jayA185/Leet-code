9d238a63f10d188f170c14f117d55feb056809fd
Jay pandhurnekar <jaywpandhurnekar2006@jdcoem.ac.in>
2026-08-23 09:46:57 +0530

    Time: 123 ms (14.70%) | Memory: 31.1 MB (73.35%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        self.stack.append(val)
+
+        if not self.minStack:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(
+                min(val, self.minStack[-1])
+            )
+
+    def pop(self):
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.minStack[-1]
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
